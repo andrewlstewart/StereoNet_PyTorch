8e88bdf47401eec670f854b56daabd7abed2a6da
Andrew Stewart <andrewlstewart@gmail.com>
2023-04-08 17:05:29 -0400

    Fixing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,10 +11,10 @@ def models() -> List[Tuple[StereoNet, int]]:
     Instantiates a bunch of models so we can test their output sizes.
     """
     models = [
-        (StereoNet(k_downsampling_layers=3, k_refinement_layers=1), 398978),  # Tuple of (model, number of trainable parameters)
-        (StereoNet(k_downsampling_layers=4, k_refinement_layers=1), 424610),
-        (StereoNet(k_downsampling_layers=3, k_refinement_layers=3), 624644),
-        (StereoNet(k_downsampling_layers=4, k_refinement_layers=3), 650276)
+        (StereoNet(in_channels=3, k_downsampling_layers=3, k_refinement_layers=1), 398978),  # Tuple of (model, number of trainable parameters)
+        (StereoNet(in_channels=3, k_downsampling_layers=4, k_refinement_layers=1), 424610),
+        (StereoNet(in_channels=3, k_downsampling_layers=3, k_refinement_layers=3), 624644),
+        (StereoNet(in_channels=3, k_downsampling_layers=4, k_refinement_layers=3), 650276)
     ]
 
     for (model, _) in models:
